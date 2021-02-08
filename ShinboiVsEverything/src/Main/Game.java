be15fdf0ba4e58798d9cd4dd0dae27977267903e
andrewruiz928 <andrewruiz928@gmail.com>
2021-02-08 00:00:32 -0500

    Shinboi Game Push

---
+++ ShinboiVsEverything/src/Main/Game.java
@@ -0,0 +1,18 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class Game {
+    
+    @SuppressWarnings("rawtypes")
+	public static void main(String[] args) {
+        
+        JFrame window = new JFrame("Shinobi vs. Everything");
+        window.setContentPane(new GamePanel());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true);
+        window.pack();
+        window.setVisible(true);
+        
+    }
+}
---
+++ ShinboiVsEverything/src/module-info.java
@@ -0,0 +1,3 @@
+module ShinboiVsEverything {
+	requires java.desktop;
+}
